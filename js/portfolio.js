8185e13238c2ae1750233dd21bd9f09d44e05cdf
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-05-16 23:17:41 -0400

    Added styling, media Q, and debugging

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,58 @@
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
 
+
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 })
 
+
+
 document.querySelectorAll('.nav-link').forEach(navLink => navLink.addEventListener("click", () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }))
+
+
+
+hamburger.addEventListener('click', () => {
+  const profileImg = document.querySelector('#profileImg')
+  profileImg.style.display === 'none' ? profileImg.style.display = 'block' : profileImg.style.display = 'none'
+})
+
+
+
+hamburger.addEventListener('click', () => {
+  const about = document.querySelector('.about')
+  about.style.display == 'none' ? about.style.display = 'block' : about.style.display = 'none'
+})
+
+
+
+hamburger.addEventListener('click', () => {
+  const aboutTxtBlock = document.querySelector('#aboutTxtBlock')
+  aboutTxtBlock.style.display == 'none' ? aboutTxtBlock.style.display = 'block' : aboutTxtBlock.style.display = 'none'
+})
+
+
+
+hamburger.addEventListener('click', () => {
+  const theArtwearGallery = document.querySelector('#theArtwearGallery')
+  theArtwearGallery.style.display == 'none' ? theArtwearGallery.style.display = 'block' : theArtwearGallery.style.display = 'none'
+})
+
+function sendEmail(){
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "henrylee.contact@gmail.com",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
