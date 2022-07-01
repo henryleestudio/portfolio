8e299d201fa641165322febeb1137196cd32f0aa
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-07-01 16:30:37 -0400

    editing port

--- server.js
+++ server.js
@@ -16,19 +16,22 @@ app.use(express.static('public'))
 
 // app.listen(process.env.PORT || 4444) 
 
-// app.get('/', function(req, res) {
-//     res.render('index.ejs');
-// });
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
 
-// app.get('/', (req, res) => {
-//     db.collection('emailForm').find().toArray((err, result) => {
-//         if (err) return console.log(err)
-//         console.log({words: result})
-//         res.render('index.ejs', {words: result})
-//       })   
-// })
+// app.listen(4444, function() {
+//     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, 
+//         (error, client) => {
+//         if(error) {
+//             throw error;
+//         }
+//         db = client.db(dbName);
+//         console.log("Connected to `" + dbName + "`, Port: 4444");
+//     });
+// }); 
 
-app.listen(4444, function() {
+app.listen(process.env.PORT || 4444, function() {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, 
         (error, client) => {
         if(error) {
@@ -37,13 +40,12 @@ app.listen(4444, function() {
         db = client.db(dbName);
         console.log("Connected to `" + dbName + "`, Port: 4444");
     });
-}); 
+}) 
 
-app.get('/', (req, res) => {
-    db.collection('emailForm').find().toArray((err, result) => {
-        if (err) return console.log(err)
-        console.log({words: result})
-        res.render('index.ejs', {words: result})
-      })
-
-})
+// app.get('/', (req, res) => {
+//     db.collection('emailForm').find().toArray((err, result) => {
+//         if (err) return console.log(err)
+//         console.log({words: result})
+//         res.render('index.ejs', {words: result})
+//       })
+// })
