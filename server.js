712e1e0d427049599e1119c6a9842fdc4b06f81b
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-07-01 16:12:44 -0400

    adding database

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 
 var db
 
-// const url = 'mongodb+srv://henrylee:truthandlight@portfoliocluster.gxhhgea.mongodb.net/?retryWrites=true&w=majority'
-// const dbName = 'portfolioWebsite'
+const url = 'mongodb+srv://henrylee:truthandlight@portfoliocluster.gxhhgea.mongodb.net/?retryWrites=true&w=majority'
+const dbName = 'portfolioWebsite'
 
 
 app.set('view engine', 'ejs')
@@ -14,22 +14,11 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 4444) 
+// app.listen(process.env.PORT || 4444) 
 
-// {
-//     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, 
-//         (error, client) => {
-//         if(error) {
-//             throw error;
-//         }
-//         db = client.db(dbName);
-//         console.log("Connected to `" + dbName + "`, Port: 4444");
-//     });
-// }); 
-
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
+// app.get('/', function(req, res) {
+//     res.render('index.ejs');
+// });
 
 // app.get('/', (req, res) => {
 //     db.collection('emailForm').find().toArray((err, result) => {
@@ -39,49 +28,22 @@ app.get('/', function(req, res) {
 //       })   
 // })
 
-// app.post('/checkWord', (req, res) => {
-//   console.log(req.body.word)
-//   let word = `${req.body.word}`
-//   let wordPal = word.toLowerCase().split('').reverse().join('')
-          
-//   if ( word.toLowerCase() === wordPal) {
-//       conclusion = 'Yes! A palindrome'
-//   } else {
-//       conclusion = 'This is NOT what we want!'
-//   }
+app.listen(4444, function() {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, 
+        (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`, Port: 4444");
+    });
+}); 
 
-//   db.collection('words').insertOne({word: req.body.word, outcome: conclusion})
-//       .then(result => {
-//         console.log('saved to database')
-//         res.redirect('/')
-//       })
-//       .catch(error => console.error(error))
-//   })
+app.get('/', (req, res) => {
+    db.collection('emailForm').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        console.log({words: result})
+        res.render('index.ejs', {words: result})
+      })
 
-//   app.delete('/deleteWord', (req, res) => {
-//     db.collection('words').findOneAndDelete({
-//         word: req.body.word,
-//     }, 
-    
-//     (err, result) => {
-//       if (err) return res.send(500, err)
-//       res.send('word deleted!')
-//     })
-//   })
-
-//   app.put('/upDate', (req, res) => {
-//     db.collection('words').findOneAndUpdate(
-//       { word: req.body.word },
-//       {
-//         $set: {
-//         //   word: req.body.word,
-//           outcome: req.body.outcome
-//         }
-//       },
-//       {
-//         upsert: false
-//       }
-//     )
-//       .then(result => res.json('Success'))
-//       .catch(error => console.error(error))
-//   })
+})
